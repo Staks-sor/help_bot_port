f5edf09562d3de8403bb2bb1865cd0d6b8ccb036
staks <stas.sor@gmail.com>
2024-07-12 18:55:44 +0300

    начал создавать бота, которого сразу же буду и на прод кидать)

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
-print("привет")
-print("ипаиа")
-print("nvnvnvbnvbnvb")
-print("nvnvnvbnvbnvb")
-print("nvnvnvbnvbnvb")
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token="5376178981:AAGRadPVp1o06jrVFSxTxm9tV8PIcH0d3V8")
+# Диспетчер
+dp = Dispatcher()
+
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
