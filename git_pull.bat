749338cde6021525dac597504798e05c31f4aa94
staks <stas.sor@gmail.com>
2024-07-15 23:37:16 +0300

    так так 1

--- git_pull.bat
+++ git_pull.bat
@@ -1,9 +1,10 @@
 @echo off
+chcp 65001 > nul
+
 set REMOTE_USERNAME=staks
 set REMOTE_IP=176.123.165.113
 set GITHUB_REPO=gh repo clone Staks-sor/help_bot_port
 
 echo Подключение к удаленной машине %REMOTE_IP%...
-ssh %REMOTE_USERNAME%@%REMOTE_IP% "cd ~/help_bot_port && git pull"
-
+ssh %REMOTE_USERNAME%@%REMOTE_IP% "if [ -d ~/help_bot_port ]; then echo 'Папка help_bot_port существует'; else echo 'Папка help_bot_port не существует'; fi"
 echo Сессия закрыта
--- main.py
+++ main.py
@@ -25,7 +25,7 @@ async def cmd_start(message: types.Message):
     kb = [
         [
             types.KeyboardButton(text="Регистрация[eq[e[qe[eq[eq[eq"),
-            types.KeyboardButton(text="Без регистрации")
+            types.KeyboardButton(text="Без регистрацииииимимимимимимим")
         ],
     ]
     keyboard = types.ReplyKeyboardMarkup(
