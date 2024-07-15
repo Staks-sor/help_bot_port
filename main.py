8d5c953856ec6676f8ddb91574de224d41b55aea
staks <stas.sor@gmail.com>
2024-07-15 17:12:35 +0300

    начинаем писать бота) наконец таки, парвда проблемы с ci\cd решить не получилось, не понятно...

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 
+import os
+from dotenv import load_dotenv
+from Description.Description import greetings_message
+
+
+
+
+
+load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 # Объект бота
-bot = Bot(token="5376178981:AAGRadPVp1o06jrVFSxTxm9tV8PIcH0d3V8")
+bot = Bot(token=BOT_TOKEN)
 # Диспетчер
 dp = Dispatcher()
 
@@ -14,7 +24,7 @@ dp = Dispatcher()
 # Хэндлер на команду /start
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Hello!k")
+    await message.answer(f"{greetings_message}")
 
 
 # Запуск процесса поллинга новых апдейтов
