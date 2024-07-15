85b3488527e3a415d59fbfb6e8840da8e7e7e351
staks <stas.sor@gmail.com>
2024-07-15 19:02:34 +0300

    продолжаем простые движения

--- main.py
+++ main.py
@@ -2,14 +2,11 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
-
+from aiogram.enums.dice_emoji import DiceEmoji
 import os
 from dotenv import load_dotenv
 from Description.Description import greetings_message
-
-
-
-
+from aiogram import F
 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
@@ -25,6 +22,29 @@ dp = Dispatcher()
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     await message.answer(f"{greetings_message}")
+    kb = [
+        [
+            types.KeyboardButton(text="Регистрация"),
+            types.KeyboardButton(text="Без регистрации")
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder="Выберите способ подачи",
+
+    )
+    await message.answer("vfdgdf", reply_markup=keyboard)
+
+
+@dp.message(F.text.lower() == "регистрация")
+async def with_puree(message: types.Message):
+    await message.reply("Отличный выбор!")
+
+
+@dp.message(F.text.lower() == "без регистрации")
+async def without_puree(message: types.Message):
+    await message.reply("Так невкусно!")
 
 
 # Запуск процесса поллинга новых апдейтов
