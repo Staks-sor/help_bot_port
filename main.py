86472e3bff0621a4bf5f21a73597b3dc815d9d0b
staks <stas.sor@gmail.com>
2024-07-16 00:30:05 +0300

    пошел в обход)

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ async def cmd_start(message: types.Message):
     kb = [
         [
             types.KeyboardButton(text="Регистрация[eq[e[qe[eq[eq[eq"),
-            types.KeyboardButton(text="Беmmmmm")
+            types.KeyboardButton(text="Беmmmmm6565465465464")
         ],
     ]
     keyboard = types.ReplyKeyboardMarkup(
