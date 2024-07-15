93b11059026e7a2bf9f9acbdb2ec55c4dd771d29
staks <stas.sor@gmail.com>
2024-07-15 23:26:51 +0300

    так так 1

--- git_pull.bat
+++ git_pull.bat
@@ -4,8 +4,6 @@ set REMOTE_IP=176.123.165.113
 set GITHUB_REPO=gh repo clone Staks-sor/help_bot_port
 
 echo Подключение к удаленной машине %REMOTE_IP%...
-ssh %REMOTE_USERNAME%@%REMOTE_IP%
+ssh %REMOTE_USERNAME%@%REMOTE_IP% "cd ~/help_bot_port && git pull"
 
-echo Обновление данных из репозитория GitHub...
-cd help_bot_port
-git pull
+echo Сессия закрыта
