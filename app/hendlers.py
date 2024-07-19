cf91b2f105009b89b0f4000b85241d1c41b4786f
staks <stas.sor@gmail.com>
2024-07-19 15:06:28 +0300

    изучаю айограм 3

--- app/hendlers.py
+++ app/hendlers.py
@@ -1,9 +1,9 @@
 from aiogram.filters.command import Command, CommandStart
 
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 
 from Description.Description import greetings_message
-from aiogram import F, types, Router
+from aiogram import F, Router
 
 import app.keyboards as kb
 
@@ -13,7 +13,7 @@ router = Router()
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer(f"{greetings_message}", reply_markup=kb.settings)
+    await message.answer(f"{greetings_message}", reply_markup=kb.in_main)
 
 
 @router.message(Command("help"))
@@ -31,3 +31,9 @@ async def photo_reply(message: Message):
 async def get_photo(message: Message):
     await message.answer_photo(
         photo="AgACAgIAAxkBAAI3O2aY9kFJu8teSjRrCn7UGkQQ2e92AAJH4TEbF9vASDDohaJ1zzGpAQADAgADbQADNQQ")
+
+
+@router.callback_query(F.data == "catalog")
+async def catalog(callback: CallbackQuery):
+    await callback.answer("Вы выбрали каталог игр")
+    await callback.message.edit_text('ghbdtn', reply_markup=await kb.inline_cars())
--- app/keyboards.py
+++ app/keyboards.py
@@ -1,10 +1,25 @@
 from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
                            InlineKeyboardMarkup, InlineKeyboardButton)
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 main = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="Каталог")],
                                      [KeyboardButton(text="Корзина"), KeyboardButton(text="Контакты")]
                                      ], resize_keyboard=True,
                            input_field_placeholder="Выберете пунк меню")
 
+in_main = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Каталог", callback_data="catalog")],
+    [InlineKeyboardButton(text="Корзина", callback_data=f"basket"), InlineKeyboardButton(text="Контакты", callback_data=f"contacs")]
+], resize_keyboard=True,
+    input_field_placeholder="Выберете пунк меню")
 
-settings = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="игра", url="stas-sor.online")]])
+settings = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="игра", callback_data="list game")]])
+
+game = ["самолет", "танки", "кукуха"]
+
+
+async def inline_cars():
+    keyboard = InlineKeyboardBuilder()
+    for games in game:
+        keyboard.add(InlineKeyboardButton(text=games, callback_data=f"games_{games}"))
+    return keyboard.adjust(2).as_markup()
