accaf7583001e7b65251d4fd6b2569668fc03cf9
staks <stas.sor@gmail.com>
2024-07-25 13:05:12 +0300

    Написал пример создания пользователя емеил. Осталось в основном боте сделать регистрацию и передавать данные сюда. Так у пользователей телеграмма будет своя почта)  3

---
+++ create_users_for_iRedMail/create_users.py
@@ -0,0 +1,20 @@
+import subprocess
+
+create_users_teleg_bot_username = 'username'
+create_users_teleg_bot_pass = 'pass'
+# Установка параметров
+username = f'{create_users_teleg_bot_username}'
+password = f'{create_users_teleg_bot_pass}'
+domain = 'stas-sor.ru'
+quota = 200
+
+# Создание SQL-команды
+sql_command = f"bash create_mail_user_SQL.sh {username}@{domain} '{password}' > /tmp/user.sql"
+
+# Выполнение SQL-команды
+subprocess.run(sql_command, shell=True)
+
+# Выполнение SQL-команды в базе данных
+subprocess.run("su - postgres", shell=True)
+subprocess.run("psql -d vmail", shell=True)
+subprocess.run("\\i /tmp/user.sql", shell=True)
